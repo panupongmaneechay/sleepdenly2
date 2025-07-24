5318adbaf421cb3985e4df059db3ec2396ac4b57
panupongmaneechay <panupongmaneechay@hotmail.com>
2025-07-24 21:40:13 +0700

    add

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,19 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // ทำให้สามารถเข้าถึงได้จากภายนอก container
+    port: 5173,
+  }
 })
