5bb082f0fd94e6090a81d0e59797ea15e070f588
panupongmaneechay <panupongmaneechay@hotmail.com>
2025-08-19 15:29:29 +0700

    add scale card

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -15,7 +15,7 @@ import GameLogo from './components/GameLogo';
 
 // const socket = io("http://localhost:3001");
 // const socket = io("http://192.168.1.102:3001"); 
-const socket = io("http://172.18.0.3:3001"); 
+const socket = io("http://10.30.16.104:3001"); 
 
 function App() {
   const [view, setView] = useState('home');
